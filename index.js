ff3d2896bb24b77d7694d2e09552274317d702b6
alexatbowst <alex@bowst.com>
2023-04-28 17:12:54 -0400

    ws implementation, but will migrate to socket.io because we need to better handle presence

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ const WebSocket = require('ws');
 const app = express();
 const server = http.createServer(app);
 
-// const wsServer = new WebSocketServer({ server });
-const wsServer = new WebSocket.Server({ server });
+// const ws = new WebSocketServer({ server });
+const wss = new WebSocket.Server({ server });
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const openAi = require('./services/openai');
@@ -29,13 +29,60 @@ app.get('/newGame', async (req, res) => {
   }
 });
 
+// app.post('/signIn', async (req, res) => {
+//   console.log('req: ', req);
+// });
+
 // I'm maintaining all active connections in this object
-const clients = {};
+let clients = {};
+let players = [];
 
 // A new client connection request received
-wsServer.on('connection', function (ws) {
+wss.on('connection', function (ws) {
   // Generate a unique code for every user
   console.log(`Recieved a new connection.`);
+
+  ws.send(JSON.stringify({ message: 'We see you' }));
+
+  ws.on('message', (data, isBinary) => {
+    // RULE: Send only JSON friendly data so it can be parsed without a try/catch here :)
+    const message = isBinary ? data : JSON.parse(data.toString());
+
+    console.log('message', message);
+    console.log('message type', typeof message);
+
+    // const test = JSON.parse(message);
+    // console.log('test: ', test);
+
+    const handleSend = (data) => {
+      const payload = JSON.stringify(data);
+      console.log('payload: ', payload);
+      ws.send(payload);
+    };
+
+    if (!message.type) {
+      return;
+    }
+    if (message.type == 'signIn' && message.user) {
+      const email = message.user.email;
+      const name = message.user?.name || email;
+      ws.email = email;
+      // clients.push(ws);
+      clients[email] = ws;
+      console.log('players: ', players);
+      players.push({
+        name,
+        email,
+      });
+      console.log('players: ', players);
+      ws.send(JSON.stringify({ type: 'players', data: players }));
+    }
+  });
+
+  ws.on('close', (code, reason) => {
+    delete players[ws.email];
+    console.log('ws.email: ', ws.email);
+  });
 });
 
 app.listen(port, () => console.log(`express listening on port ${port}`));
