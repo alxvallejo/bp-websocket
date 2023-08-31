1471254f2d5e41756d817e1a9824d1b22cc8c295
alexatbowst <alex@bowst.com>
2023-08-31 19:09:34 -0400

    Testing spanish

--- services/openai.js
+++ services/openai.js
@@ -9,7 +9,7 @@ const configuration = new Configuration({
 const openai = new OpenAIApi(configuration);
 
 const newGamePrompt = (category) => {
-  return `You can only respond in JSON format. Generate an interesting trivia question on the topic of ${category}, returning 4 possible answers under the property "options" along with whether each option is correct using a boolean property "isAnswer". Each option is in the format of "{ option, isAnswer }". Only one answer can be correct. The trivia question should be returned on the property "question". Additionally, provide a 2-3 sentence explanation for the answer using the property "answerContext". Also, add a property called "keywords" and provide 1 sentence explaining the answer. Remove all line breaks from the json response and do not add a prefix to the json. Do not stringify the json. Make sure the json string is wrapped in brackets.`;
+  return `You can only respond in JSON format. Generate an interesting trivia question in Spanish on the topic of ${category}, returning 4 possible answers in Spanish under the property "options" along with whether each option is correct using a boolean property "isAnswer". Each option is in the format of "{ option, isAnswer }". Only one answer can be correct. The trivia question should be returned on the property "question". Additionally, provide a 2-3 sentence explanation for the answer using the property "answerContext". Also, add a property called "keywords" and provide 1 sentence explaining the answer. Remove all line breaks from the json response and do not add a prefix to the json. Do not stringify the json. Make sure the json string is wrapped in brackets.`;
 };
 
 const newGame = async (category) => {
